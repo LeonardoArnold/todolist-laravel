cea2e09a92d2956befa957f983e5989d22950f50
Leonardo Arnold <leonardobandasshow@gmail.com>
2026-06-20 16:55:41 -0300

    Corrige posicao da rota de migrations

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return redirect()->route('tarefas.index');
+});
+
 Route::get('/rodar-migrations-9382', function () {
     \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
     return '<pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
-});    
 });
 
 Route::put('/tarefas/{tarefa}/toggle', [TaskController::class, 'toggle'])->name('tarefas.toggle');
