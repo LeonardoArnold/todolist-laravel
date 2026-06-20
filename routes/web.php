2892420daa8282c1688b39b3fc7d7d1f4881e1df
Leonardo Arnold <leonardobandasshow@gmail.com>
2026-06-20 17:02:52 -0300

    Remove rota temporaria de migrations

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ Route::get('/', function () {
     return redirect()->route('tarefas.index');
 });
 
-Route::get('/rodar-migrations-9382', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-    return '<pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
-});
 
 Route::put('/tarefas/{tarefa}/toggle', [TaskController::class, 'toggle'])->name('tarefas.toggle');
 Route::resource('tarefas', TaskController::class);
